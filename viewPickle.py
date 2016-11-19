1adc4965f761cabbc29368259b86efd227e18d9f
brett <whitford.15@osu.edu>
2016-11-19 17:36:21 -0500

    commented viewPickle

--- viewPickle.py
+++ viewPickle.py
@@ -1,6 +1,6 @@
-import pandas as pd
+import pandas as pd #import the panda type
 perf = pd.read_pickle('dma.pickle') # read in perf DataFrame
-print perf.head(252)
+print perf.head(252) #prints to console first 252 lines (one year of trading)
 
-#attempt to print to csv
+#Prints the panda to a csv file
 perf.to_csv('predictionOutput.csv')
