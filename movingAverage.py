650f9c8547b972f910fa33d703ec89603e5e8bea
brett <whitford.15@osu.edu>
2016-11-19 19:53:57 -0500

    moving average metric

--- movingAverage.py
+++ movingAverage.py
@@ -36,34 +36,4 @@ def handle_data(context, data):
            short_mavg=short_mavg[sym],
            long_mavg=long_mavg[sym])
 
-from zipline.api import order_target, record, symbol, history
-import numpy as np
-
-def initialize(context):
-    context.i = 0
-
-
-def handle_data(context, data):
-    # Skip first 300 days to get full windows
-    context.i += 1
-    if context.i < 300:
-        return
-
-    # Compute averages
-    # history() has to be called with the same params
-    # from above and returns a pandas dataframe.
-    short_mavg = history(100, '1d', 'price').mean()
-    long_mavg = history(300, '1d', 'price').mean()
-
-    # Trading logic
-    if short_mavg[0] > long_mavg[0]:
-        # order_target orders as many shares as needed to
-        # achieve the desired number of shares.
-        order_target(symbol('AAPL'), 100)
-    elif short_mavg[0] < long_mavg[0]:
-        order_target(symbol('AAPL'), 0)
-
-    # Save values for later inspection
-    record(AAPL=data[symbol('AAPL')].price,
-           short_mavg=short_mavg[0],
-           long_mavg=long_mavg[0])
+    record('brett', context.i)
--- viewPickle.py
+++ viewPickle.py
@@ -1,6 +1,6 @@
 import pandas as pd #import the panda type
 perf = pd.read_pickle('dma.pickle') # read in perf DataFrame
-print perf.head(252) #prints to console first 252 lines (one year of trading)
+print perf.head() #prints to console first 252 lines (one year of trading)
 
 #Prints the panda to a csv file
 perf.to_csv('predictionOutput.csv')
